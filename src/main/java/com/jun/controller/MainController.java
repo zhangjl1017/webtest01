8717bca518eb405e5e90e0f35094fd49109cb3b9
zhangjl1017 <ithlw@qq.com>
2016-10-08 12:23:24 +0800

    web 初始化方法

---
+++ src/main/java/com/jun/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.jun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by NIT on 2016/10/8.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
